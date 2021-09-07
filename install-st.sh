69e2d8e0bbfa35ccf2f82ed4d900563de9477e2f
____marcell <0000marcell@gmail.com>
2021-09-07 20:26:41 +0000

    download funcionality

--- install-st.sh
+++ install-st.sh
@@ -4,7 +4,7 @@ cp ./.Xresources ~/
 xrdb ~/.Xresources
 
 cd ~/
-curl https://codeload.github.com/0000marcell/st/zip/heads/master -o st.zip
-unzip ./st.zip
-cd st-master
+curl https://codeload.github.com/0000marcell/st/zip/heads/master -o ~/st.zip
+unzip ~/st.zip
+cd ~/st-master
 sudo make clean install
