accb8c18e4905718d2aad07b4f117fcb5c73dbce
marcell cruz <0000marcell@gmail.com>
2022-06-12 10:23:28 -0300

    more changes

---
+++ updatedotfiles.sh
@@ -0,0 +1,13 @@
+# update i3 configs
+cp -r ~/.config/i3/* ./config/i3
+cp -r ~/.config/i3status/* ./config/i3status
+cp ~/.config/i3/background.jpg ./background.jpg
+
+# update nvim
+cp -R ~/.config/nvim/* ./config/nvim
+
+# update x configs
+cp ~/.xinitrc ./
+cp ~/.Xmodmap ./
+cp ~/.Xmodmapbr ./
+cp ~/.Xresources ./
