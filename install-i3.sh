169610ec6dd93a0fbac44072e33ee4857653bf2d
____marcell <0000marcell@gmail.com>
2022-07-23 15:12:21 +0000

    added scripts folder added dunst for notifications

--- install-i3.sh
+++ install-i3.sh
@@ -1,6 +1,6 @@
 # install-x needs to run first
 sudo pacman -Syuu --noconfirm
-sudo pacman -S --noconfirm i3-wm feh i3blocks
+sudo pacman -S --noconfirm i3-wm feh i3blocks dunst
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3blocks
 cp -r ./config/i3/* ~/.config/i3
