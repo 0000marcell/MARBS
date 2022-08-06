676f90ed3542e9362ac687f2f65eb116be164212
____marcell <0000marcell@gmail.com>
2022-08-06 13:23:00 +0000

    i3wm automation added

---
+++ i3wm-automation/simple_one_terminal/open.sh
@@ -0,0 +1,3 @@
+#i3-msg 'workspace 5:Slack; exec brave https://app.slack.com/client/T042FTU5U/C02UCM45WV9'
+i3-msg 'workspace 1:Code; append_layout ./workspace-1.json'
+st -f "Liberation Mono-16" &
---
+++ i3wm-automation/work/open.sh
@@ -0,0 +1,2 @@
+#i3-msg 'workspace 5:Slack; exec brave https://app.slack.com/client/T042FTU5U/C02UCM45WV9'
+i3-msg 'workspace 1:Code; append_layout ./one-terminal.json, workspace 2:Code; append_layout ./one-terminal.json, workspace 3:Code; append_layout ./one-terminal.json'
