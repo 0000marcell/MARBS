eff3c6e7da85d1df8ea9f8b0b82112de3001e69f
____marcell <0000marcell@gmail.com>
2023-01-28 20:24:03 +0000

    more scripts added

--- install-scripts.sh
+++ install-scripts.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/scripts
 
-cp ./togglekb ~/scripts 
-
+cp ./scripts/* ~/scripts/
+curl https://raw.githubusercontent.com/0000marcell/checkbatcapacity/master/checkbatcapacity -o ~/scripts/checkbatcapacity
