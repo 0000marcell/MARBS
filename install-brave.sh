4a62d209d9bd947af1844dfc1915e7dc89cf4cff
Marcell Cruz <0000marcell@gmail.com>
2023-12-04 18:08:41 -0300

    install brave updated

--- install-brave.sh
+++ install-brave.sh
@@ -2,4 +2,4 @@ sudo pacman -Syuu --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S --save --nocleanmenu --nodiffmenu brave
+yay -S --save --nocleanmenu --nodiffmenu brave-bin
