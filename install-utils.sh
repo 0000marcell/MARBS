ca08fec06765c41e9081259fad59d08282fba24a
____marcell <0000marcell@gmail.com>
2024-05-05 12:18:57 +0000

    more scripts

--- install-utils.sh
+++ install-utils.sh
@@ -1,1 +1,11 @@
 sudo pacman -Syuu --noconfirm xclip highlight zathura ranger procs
+
+# Install espansio
+# Create the $HOME/opt destination folder
+mkdir -p ~/opt
+# Download the AppImage inside it
+wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.2.1/Espanso-X11.AppImage'
+# Make it executable
+chmod u+x ~/opt/Espanso.AppImage
+# Create the "espanso" command alias
+sudo ~/opt/Espanso.AppImage env-path register
