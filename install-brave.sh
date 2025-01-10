cc2195c55159c2cc7d175480706bcd8a80452949
____marcell <0000marcell@gmail.com>
2025-01-10 00:34:31 +0000

    fix brave installation

--- install-brave.sh
+++ install-brave.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syuu --noconfirm
+sudo pacman -S git debugedit fakeroot
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S --save --nocleanmenu --nodiffmenu brave-bin
+yay -S --save brave-bin
