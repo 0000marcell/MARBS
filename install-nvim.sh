2d846bb6e8e4b6e79cc07b8bc3ac59b9fc4c8c4c
Marcell Cruz <0000marcell@gmail.com>
2025-02-09 13:07:15 -0300

    neovim install additions

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
 # install nvim
-sudo pacman -Sy --noconfirm neovim
+sudo pacman -Sy --noconfirm neovim xclip tree-sitter
 
-# copy config files
-cp -R ./config/nvim ~/.config
-
-# install plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# install lazy vim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+# you need to open vim then
+# clone dotfiles
+# cd $HOME
+# rm -rf Dotfiles
+# git clone https://github.com/0000marcell/Dotfiles.git
+# cp Dotfiles/init.lua $HOME/init.lua
+# ln -s $HOME/Dotfiles/* $HOME/.config
